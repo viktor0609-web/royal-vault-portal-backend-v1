09cbdfcd2fd02e58c8afc1239e84e19aba849350
Viktor <viktor@royallegalsolutions.com>
2025-09-22 07:47:22 +0300

    updated routing

--- routes/dealRoutes.js
+++ routes/dealRoutes.js
@@ -11,10 +11,12 @@ import { protect, authorize } from '../middleware/authMiddleware.js';
   
 const router = express.Router();
 
-router.post('/', protect, authorize('admin'), createDeal); // Create a new deal
 router.get('/', protect, getAllDeals); // Get all deals
 router.get('/filter', protect, filterDeals); // Filter deals
 router.get('/:dealId', protect, getDealById); // Get a deal by ID
+
+
+router.post('/', protect, authorize('admin'), createDeal); // Create a new deal
 router.put('/:dealId', protect, authorize('admin'), updateDeal); // Update a deal
 router.delete('/:dealId', protect, authorize('admin'), deleteDeal); // Delete a deal
 
