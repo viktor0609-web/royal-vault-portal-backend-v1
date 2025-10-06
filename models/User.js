7b4ffb0a58b664be7eddd7e9ed88f5e6cd4c37a2
Viktor <viktor@royallegalsolutions.com>
2025-10-06 18:15:52 +0300

    updated profile page

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
   phone: { type: String, required: true },
   password: { type: String, required: true },
