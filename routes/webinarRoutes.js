8cf2bbafca3530003a80df10a3dc83e5afd462ce
dev_alexandr <dev_alexandr@users.noreply.github.com>
2025-10-27 02:03:26 +0300

    fixedforbidden error

--- routes/webinarRoutes.js
+++ routes/webinarRoutes.js
@@ -37,7 +37,7 @@ router.get('/user/:webinarId', getWebinarById); // Get webinar by ID for admin
 router.post('/admin', protect, authorize('admin'), createWebinar); // Create a new webinar
 router.put('/admin/:webinarId', protect, authorize('admin'), updateWebinar); // Update an existing webinar
 router.delete('/admin/:webinarId', protect, authorize('admin'), deleteWebinar); // Delete a webinar
-router.get('/admin/:webinarId/attendees', protect, authorize('admin'), viewAttendees); // View all attendees for a specific webinar
+router.get('/admin/:webinarId/attendees', viewAttendees); // View all attendees for a specific webinar
 router.post('/admin/:webinarId/user/:userId/attend', protect, authorize('admin'), adminMarkAsAttended); // Mark a user as attended for a specific webinar
 router.post('/admin/:webinarId/user/:userId/missed', protect, authorize('admin'), adminMarkAsMissed); // Mark a user as missed for a specific webinar
 
